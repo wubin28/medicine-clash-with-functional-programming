cbcd8381ada542f07eed46e8ec54db0f06185107
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-22 15:08:18 +0000

    6

--- sandbox/MedicineClashTest.java
+++ sandbox/MedicineClashTest.java
@@ -7,6 +7,12 @@ public class MedicineClashTest {
     //       given two clashed medicines were being taken during 
     //       a given period of time (defaults to the last 90 days).
 
+    // User story: As a Health Insurer,
+    //             I want to be able to search for patients who have 
+    //                 a medicine clash,
+    //             So that I can alert their doctors and get their 
+    //                 prescriptions changed.
+
     @Test
     public void should() {
         assertEquals(1, 1);
