f150d0e0a10c9b21f2531989674e37ad5be2a5b5
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-22 05:00:04 +0000

    4

--- sandbox/MedicineClashTest.java
+++ sandbox/MedicineClashTest.java
@@ -2,6 +2,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class MedicineClashTest {
+
+    // GOAL: To find the a collection of days on which all the 
+             given two clashed medicines were being taken during 
+             a given period of time (defaults to the last 90 days).
+
     @Test
     public void should() {
         assertEquals(1, 1);
