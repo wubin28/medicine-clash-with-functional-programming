f2351713ef417a88cd5dbcedbb6a78ebe60f8cc7
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-22 18:08:57 +0000

    7

--- sandbox/MedicineClashTest.java
+++ sandbox/MedicineClashTest.java
@@ -13,6 +13,14 @@ public class MedicineClashTest {
     //             So that I can alert their doctors and get their 
     //                 prescriptions changed.
 
+    // TODO: no_clash_when_not_taking_both_medicines
+    // TODO: no_clash_when_no_overlap
+    // TODO: two_medicines_taken_in_a_partially_overlapping_period
+    // TODO: two_medicines_taken_overlapping_start_of_period
+    // TODO: two_medicines_taken_overlapping_current_date
+    // TODO: clash_when_medicines_taken_continuously
+    // TODO: clash_when_one_medicine_taken_on_some_of_the_days
+
     @Test
     public void should() {
         assertEquals(1, 1);
