4caa247216d0a4f721a5c64b6f96a3386287ae16
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-26 06:02:20 +0000

    83

--- sandbox/MedicineClash.java
+++ sandbox/MedicineClash.java
@@ -8,8 +8,18 @@ public class MedicineClash {
             List<Customer> customers,
             long daysBeforeToday, 
             LocalDate now) {
+
+        // TODO: cover the following
         return customers.stream()
-            .filter(customer -> customer.medicines.size() == 2)
+            .filter(customer -> customer.medicines.size() == 2 &&
+                ((customer.medicines.get(0).name == "Codeine" &&
+                    customer.medicines.get(1).name == "Prozac") ||
+                 (customer.medicines.get(0).name == "Prozac" &&
+                    customer.medicines.get(1).name == "Codeine")
+                ) &&
+                customer.medicines.get(0).prescriptions.size() >= 1 &&
+                customer.medicines.get(1).prescriptions.size() >= 1
+            )
             .filter(customer -> 
                 MedicineClash.isOverlapped(customer, daysBeforeToday, now))
             .collect(Collectors.toList());
@@ -19,6 +29,12 @@ public class MedicineClash {
             Customer customer,
             long daysBeforeToday, 
             LocalDate now) {
-        return false;
+        LocalDate startA = customer.medicines.get(0).;
+        LocalDate endA = getEndDate(medicine);
+        LocalDate startB = getStartDate(clashingMedicine);
+        LocalDate endB = getEndDate(clashingMedicine);
+        
+        return !(startA.isAfter(endB) || endA.isBefore(startB)) 
+            ? true : false;
     }
 }
