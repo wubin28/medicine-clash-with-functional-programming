7ee78af387ebd5c90c9c7ddc2f880a8dd379b92b
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-26 06:24:12 +0000

    86

--- sandbox/MedicineClash.java
+++ sandbox/MedicineClash.java
@@ -6,16 +6,18 @@ import java.time.LocalDate;
 public class MedicineClash {
     public static List<Customer> of(
             List<Customer> customers,
+            String medicineName,
+            String clashingMedicineName,
             long daysBeforeToday, 
             LocalDate now) {
 
         // TODO: tests covering the following
         return customers.stream()
             .filter(customer -> customer.medicines.size() == 2 &&
-                ((customer.medicines.get(0).name == "Codeine" &&
-                    customer.medicines.get(1).name == "Prozac") ||
-                 (customer.medicines.get(0).name == "Prozac" &&
-                    customer.medicines.get(1).name == "Codeine")
+                ((customer.medicines.get(0).name == medicineName &&
+                    customer.medicines.get(1).name == clashingMedicineName) ||
+                 (customer.medicines.get(0).name == clashingMedicineName &&
+                    customer.medicines.get(1).name == medicineName)
                 ) &&
                 customer.medicines.get(0).prescriptions.size() >= 1 &&
                 customer.medicines.get(1).prescriptions.size() >= 1
