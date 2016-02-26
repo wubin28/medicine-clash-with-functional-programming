b661acf83425c19dea82cf2dbd79271c8c590d53
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-26 02:16:16 +0000

    62

--- sandbox/Prescription.java
+++ sandbox/Prescription.java
@@ -1,12 +1,17 @@
 import java.time.LocalDate;
 
 public class Prescription {
+    public LocalDate dispenseDate;
+    public int daysSupply;
+    public String doctorName;
+
     public Prescription(LocalDate dispenseDate, int daysSupply, 
         String doctorName) {
     }
 
     @Override
     public String toString() {
-        return "Prescription dispense date is " + this.dispenseDate + "for " + this.daysSupply + " days";
+        return "Prescription written on " + this.dispenseDate + 
+            " for " + this.daysSupply + " days by " + this.doctorName;
     }
 }
