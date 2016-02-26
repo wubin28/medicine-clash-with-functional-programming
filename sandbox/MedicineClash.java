072935593613e4bf4d7bf477f4eb5c93725f6672
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-26 01:35:16 +0000

    50

--- sandbox/MedicineClash.java
+++ sandbox/MedicineClash.java
@@ -1,6 +1,7 @@
 import java.util.Set;
 import java.util.HashSet;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class MedicineClash {
     public static Set<Customer> of(Set<Customer> customers) {
