164761e965667d73b50c384329b1cb292baedbda
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-26 02:10:27 +0000

    60

--- sandbox/Medicine.java
+++ sandbox/Medicine.java
@@ -1,6 +1,16 @@
 import java.util.List;
 
 public class Medicine {
+    public String name;
+    public List<Prescription> prescriptions;
+
     public Medicine(String name, List<Prescription> prescriptions) {
+        this.name = name;
+        this.prescriptions = prescriptions;
+    }
+
+    @Override
+    public String toString() {
+        return "Medicine " + this.name + " with prescriptions " + this.prescriptions;
     }
 }
