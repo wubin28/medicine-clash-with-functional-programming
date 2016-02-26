790302ec628dbdad67893b58c83bc791161a67f8
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-26 06:40:42 +0000

    89

--- sandbox/Customer.java
+++ sandbox/Customer.java
@@ -15,4 +15,28 @@ public class Customer {
     public String toString() {
         return "Customer " + this.name + " (" + this.id + ") taking medicines " + this.medicines;
     }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 31). // two randomly chosen prime numbers
+            // if deriving: appendSuper(super.hashCode()).
+            append(id).
+            append(name).
+            toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+       if (!(object instanceof Customer))
+            return false;
+        if (object == this)
+            return true;
+
+        Customer customer = (Customer) object;
+        return new EqualsBuilder().
+            // if deriving: appendSuper(super.equals(object)).
+            append(id, customer.id).
+            append(name, rhs.name).
+            isEquals();
+    }
 }
