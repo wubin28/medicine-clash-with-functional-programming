807c7be60fb4f033c7efa27e9d0b16704ec9ab19
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-26 02:39:02 +0000

    67

--- sandbox/MedicineClash.java
+++ sandbox/MedicineClash.java
@@ -3,7 +3,8 @@ import java.util.stream.Stream;
 import java.util.stream.Collectors;
 
 public class MedicineClash {
-    public static List<Customer> of(List<Customer> customers) {
+    public static List<Customer> of(List<Customer> customers,
+            long daysBeforeToday, LocalDate now) {
         return customers.stream()
             .filter(customer -> customer.medicines.size() == 2)
             .filter(customer -> customer.medicines.size() == 2)
