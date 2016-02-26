f4ef1049a47e3a17e962ecf5ca07fb09d69ce233
bee_39CC50C51D <bee@cyber-dojo.org>
2016-02-26 01:34:13 +0000

    49

--- sandbox/Customer.java
+++ sandbox/Customer.java
@@ -1,6 +1,13 @@
 import java.util.Set;
 
 public class Customer {
+    public String id;
+    public String name;
+    public Set<Medicine> medicines;
+
     public Customer(String id, String name, Set<Medicine> medicines) {
+        this.id = id;
+        this.name = name;
+        this.medicines = medicines;
     }
 }
